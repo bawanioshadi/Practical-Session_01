ef3865cd10753f966d5da15fd91cca33e9db40da
oshadi <bawanioshadi0@gmail.com>
2025-03-16 14:34:07 +0530

    third practical

--- third.java
+++ third.java
@@ -1,11 +1,32 @@
-//This is a comment.and third java program
+class One{
+    int a=10;
+    int b;
 
-import java.io.*;  //import packages
- class third{
-  public static void main(String[] ar) {
-    System.out.println("The first argument is "+ar[0]);
-    System.out.println("The second argument is "+ar[1]);
-    System.out.println(ar[0]+" "+ar[2]+" "+ar[1]);
+    void printDetails(){
+        System.out.println("From class One a="+a);
+        System.out.println("From class One b="+b);
 
-  }
+    }
+}
+class Two{
+    int a,c;
+    void printDetails(){
+        System.out.println("From class Two a="+a);
+        System.out.println("From class Two c="+c);
+
+    }
+}
+class App{
+    public static void main(String[] args){
+        One ol=new One();
+        Two tl=new Two();
+        ol.printDetails();
+        ol.b=100;
+        ol.printDetails();
+        tl.printDetails();
+        tl.a=20;
+        tl.c=10;
+        tl.printDetails();
+
+    }
 }
