c9029ad5874768f6603748699fd00e1c51e9785f
oshadi <bawanioshadi0@gmail.com>
2025-03-16 12:59:12 +0530

    first

---
+++ first.java
@@ -0,0 +1,11 @@
+//This is a comment.and first java program
+
+import java.io.*;  //import packages
+ class first{
+  public static void main(String[] args) {
+    System.out.print("Hi");
+    System.out.print("This is IT1214 Practical session 1");
+    System.out.println("This prints in new Line");
+
+  }
+}
