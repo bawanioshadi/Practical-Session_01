622ba4a565448012b5f296315f3c6e1537aec301
oshadi <bawanioshadi0@gmail.com>
2025-03-16 13:12:32 +0530

    second

---
+++ second.java
@@ -0,0 +1,10 @@
+//This is a comment.and second java program
+
+import java.io.*;  //import packages
+ class second{
+  public static void main(String[] ar) {
+    System.out.println("The first argument is "+ar[0]);
+    System.out.println("The second argument is "+ar[1]);
+
+  }
+}
---
+++ third.java
@@ -0,0 +1,11 @@
+//This is a comment.and third java program
+
+import java.io.*;  //import packages
+ class third{
+  public static void main(String[] ar) {
+    System.out.println("The first argument is "+ar[0]);
+    System.out.println("The second argument is "+ar[1]);
+    System.out.println(ar[0]+" "+ar[2]+" "+ar[1]);
+
+  }
+}
