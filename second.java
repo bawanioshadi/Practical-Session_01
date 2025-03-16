d74191061fe53c58aeafbf81c73b59cc693b0795
oshadi <bawanioshadi0@gmail.com>
2025-03-16 14:35:18 +0530

    second practical

--- second.java
+++ second.java
@@ -5,6 +5,7 @@ import java.io.*;  //import packages
   public static void main(String[] ar) {
     System.out.println("The first argument is "+ar[0]);
     System.out.println("The second argument is "+ar[1]);
+    System.out.println(ar[0]+" "+ar[2]+" "+ar[1]);
 
   }
 }
